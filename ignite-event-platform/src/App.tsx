21e2e9c86b97ca01e6bd66967e80272231946be4
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-06-21 23:13:01 -0300

    create Header and Sidebar components

--- ignite-event-platform/src/App.tsx
+++ ignite-event-platform/src/App.tsx
@@ -1,28 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
   return (
-    <ul className="text-5xl">
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
